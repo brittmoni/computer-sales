e5a76a023f5424220a19cb98638620d3c28da73b
brittmoni <brittfranklin92@gmail.com>
2019-08-07 20:51:41 -0400

    Basic rendering of React app

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const router = require('./routes');
+// const mongoose = require('mongoose');
+// const router = require('./routes');
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use('/', router);
+// app.use('/', router);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -34,9 +34,9 @@ app.use(function(req, res) {
   res.end(JSON.stringify(req.body, null, 2))
 })
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://user:password123@ds019076.mlab.com:19076/heroku_336zshph', { useNewUrlParser:true })
-  .then(() => console.log('Connected to database'))
-  .catch(err => console.log(err));
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://user:password123@ds019076.mlab.com:19076/heroku_336zshph', { useNewUrlParser:true })
+  // .then(() => console.log('Connected to database'))
+  // .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
